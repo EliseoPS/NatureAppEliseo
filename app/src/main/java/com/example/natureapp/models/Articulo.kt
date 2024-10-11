1b70df2effa6b2c88bca5d838f81b0f22a89bb39
eliseo <eliseoparedes23@gmail.com>
2024-10-10 23:36:06 -0600

    NatureAppELiseo

---
+++ app/src/main/java/com/example/natureapp/models/Articulo.kt
@@ -0,0 +1,8 @@
+package com.example.natureapp.models
+
+data class Articulo(
+    val id : Int,
+    val title : String,
+    val image : String,
+    val description : String
+)
---
+++ app/src/main/java/com/example/natureapp/utils/MockData.kt
@@ -0,0 +1,36 @@
+package com.example.natureapp.utils
+
+import com.example.natureapp.models.Articulo
+
+val articuloList = listOf(
+    Articulo(
+        id = 1,
+        title = "Bosque de Piedra de Shilin",
+        description = "https://www.chinahighlights.com/image/shilin/stone-forest1.jpg",
+        image = "https://www.clarin.com/2017/11/08/ryBW3k-yz_720x0.jpg"
+    ),
+    Articulo(
+        id = 2,
+        title = "Montañas Amarillas (Huangshan)",
+        description = "https://www.chinahighlights.com/image/huangshan/huangshan1.jpg",
+        image = "https://blog.fsl.es/wp-content/uploads/2019/10/Huangshan.-La-montan%CC%83a-amarilla-de-China-min.jpeg"
+    ),
+    Articulo(
+        id = 3,
+        title = "Parque Nacional de Zhangjiajie",
+        description = "https://www.chinahighlights.com/image/zhangjiajie/zhangjiajie2.jpg",
+        image = "https://www.viajedechina.com/pic/city/zhangjiajie/attractions/Zhangjiajie-National-Forest-Park-7.jpg"
+    ),
+    Articulo(
+        id = 4,
+        title = "Lago del Oeste (Xihu)",
+        description = "https://www.chinahighlights.com/image/hangzhou/west-lake1.jpg",
+        image = "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/27/79/b5.jpg"
+    ),
+    Articulo(
+        id = 5,
+        title = "Reserva Natural Jiuzhaigou",
+        description = "https://www.chinahighlights.com/image/jiuzhaigou/jiuzhaigou2.jpg",
+        image = "https://www.thechinaguide.com/uploads/201704/02/58e09de7172e3.jpg"
+    )
+)
